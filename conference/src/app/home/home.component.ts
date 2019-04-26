ccfb305d5c0b72bb4d5132984ec58bfecbba8dfd
christian212519 <penrod.christian7984@gmail.com>
2019-04-25 20:43:40 -0400

    Created home page

--- conference/src/app/home/home.component.ts
+++ conference/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { Conferences } from 'src/app/conferences';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  conferences: Object[] = [];
+
+  slide1: JQuery<HTMLElement>
+  slide2: JQuery<HTMLElement>
+  slide3: JQuery<HTMLElement>
+
+  dot1: JQuery<HTMLElement>
+  dot2: JQuery<HTMLElement>
+  dot3: JQuery<HTMLElement>
+
   constructor() { }
 
   ngOnInit() {
+
+    Conferences.forEach(index => {
+      if (index.id < 4) {
+        this.conferences.push(index);
+      }
+    });
+
+  }
+
+  slideIterator(index: number, direction: string): void {
+
+    this.slide1 = $("#carousel-slide:nth-child(1)");
+    this.slide2 = $("#carousel-slide:nth-child(2)");
+    this.slide3 = $("#carousel-slide:nth-child(3)");
+
+    if (direction == "next") {
+      if (index == 1) { this.slide1.hide(); this.slide2.show() }
+      else if (index == 2) { this.slide2.hide(); this.slide3.show() }
+      else { this.slide3.hide(); this.slide1.show() }
+    }
+    else if (direction == "prev") {
+      if (index == 1) { this.slide1.hide(); this.slide3.show(); }
+      else if (index == 2) { this.slide2.hide(); this.slide1.show(); }
+      else { this.slide3.hide(); this.slide2.show(); }
+    }
+
+  }
+
+  openCalendarModal(): void {
+    $('app-calendar-modal').css('visibility', 'visible');
   }
 
 }
