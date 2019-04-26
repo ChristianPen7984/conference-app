24629636f908ea15634c504284edd94d1c64c077
christian212519 <penrod.christian7984@gmail.com>
2019-04-25 20:16:24 -0400

    Set global objects and classes

---
+++ conference/src/app/conference.ts
@@ -0,0 +1,11 @@
+export class Conference {
+    id: number;
+    title: string;
+    date: string;
+    location: string;
+    image: string;
+    summary: string;
+    description: string;
+    lat: number;
+    long: number;
+}
---
+++ conference/src/conferences.ts
@@ -0,0 +1,134 @@
+export const Conferences = [
+    {
+        id: 1,
+        title: "Google IO",
+        date: "04/15/2019",
+        location: "Shoreline Amphitheatre, North Shoreline Boulevard, Mountain View, CA 94043",
+        image: "../../../assets/conference_images/googleio-conference.jpg",
+        summary: "I/O brings together developers from around the globe for talks, hands-on learning with Google experts, and a first look at Google’s latest developer products.",
+        description: "I/O brings together developers from around the globe for talks, hands-on learning with Google experts, and a first look at Google’s latest developer products. Join us for Codelabs, Sandboxes, and a chance to talk with Googlers who will be on-hand to share their insights.",
+        lat: 37.4268136,
+        long: -122.0806323
+    },
+    {
+        id: 2,
+        title: "Intel 5G Roadmap",
+        date: "04/18/2019",
+        location: "Av. Joan Carles I, 64, 08908 L'Hospitalet de Llobregat, Barcelona, Spain",
+        image: "../../../assets/conference_images/intel5g-conference.jpg",
+        summary: "Intel will showcase its latest technologies in 5G and networking at Mobile World Congress 2019. Outlining a roadmap of 5G integration and future plans.",
+        description: "Intel will showcase its latest technologies in 5G and networking at Mobile World Congress 2019. Among the demonstrations that span Intel’s full portfolio of network, cloud and client solutions are retail, immersive media and manufacturing. Mobile World Congress will be held in Barcelona in the Fira Gran Via. Intel’s booth is located in Hall 3, #3E31.",
+        lat: 41.3516029,
+        long: 2.1297575
+    },
+    {
+        id: 3,
+        title: "Black Hat 2019",
+        date: "05/03/2019",
+        location: "3950 S Las Vegas Blvd, Las Vegas, NV 89119",
+        image: "../../../assets/conference_images/blackhat-conference.jpg",
+        summary: "Now in its 22nd year, Black Hat USA is the world's leading information security event, providing attendees with the very latest in research, development and trends.",
+        description: "Now in its 22nd year, Black Hat USA is the world's leading information security event, providing attendees with the very latest in research, development and trends. Black Hat USA 2019 opens with four days of technical Trainings (August 3-6) followed by the two-day main conference (August 7-8) featuring Briefings, Arsenal, Business Hall, and more.",
+        lat: 36.0639658,
+        long: -115.1722444
+    },
+    {
+        id: 4,
+        title: "Apple Wordwide Developers Conference",
+        date: "05/03/2019",
+        location: "408 Almaden Blvd, San Jose, CA 95113",
+        image: "../../../assets/conference_images/apple-conference.jpg",
+        summary: "WWDC 2019 will deliver the latest Apple news from the keynote stage and celebrate the breakthrough work of developers who are creating new experiences.",
+        description: "WWDC 2019 will deliver the latest Apple news from the keynote stage and celebrate the breakthrough work of developers who are creating new experiences in areas including machine learning, augmented reality, health and fitness, and more. This year’s program will feature technical sessions, hands-on labs and guest speakers to provide Apple’s existing developer community and the next generation of app developers with the knowledge and tools to help turn their next great idea into reality.  ",
+        lat: 37.3279082,
+        long: -121.890006
+    },
+    {
+        id: 5,
+        title: "AWS re:Invent",
+        date: "05/21/2019",
+        location: "415 Summer St, Boston, MA 02210",
+        image: "../../../assets/conference_images/aws-conference.jpg",
+        summary: "The conference attracts Amazon’s top customers, software distribution partners (ISVs) and public cloud MSPs.",
+        description: "Amazon AWS re:Invent 2019 news involves Accenture, Alert Logic, Aqua Security, Candid Partners, Caveonix, CloudBerry Lab, Cohesity, Cylance, Datadog, Druva, Hiber, Logz.io, Mission, Orkus, Pager Duty, Qualys, SolarWinds MSP, TEKsystems, Tenable, VMware & more.",
+        lat: 36.169941,
+        long: -115.139832
+    },
+    {
+        id: 6,
+        title: "Data & AI Discovery Days",
+        date: "06/01/2019",
+        location: "Terrace Centre Centre house 114 The Terrace, Wellington, 6011, New Zealand",
+        image: "../../../assets/conference_images/azure-conference.jpg",
+        summary: "Data Discovery Days is your opportunity to explore Azure Data Services through discussions and demonstrations by the best in the field.",
+        description: "Data Discovery Days is your opportunity to explore Azure Data Services through discussions and demonstrations by the best in the field. Unlock the full potential of your business by getting an early view of the Azure Data Services roadmap, getting under the hood of everything from data ingestion to storage, advanced analytics, and automation through Artificial Intelligence, discovering how to build flexible cloud data platform that can be implemented immediately.",
+        lat: -41.2829,
+        long: 174.7842
+    },
+    {
+        id: 7,
+        title: "Google Cloud Next '19",
+        date: "06/11/2019",
+        location: "747 Howard St, San Francisco, CA 94103",
+        image: "../../../assets/conference_images/google-conference.jpg",
+        summary: "Google Cloud Next is where customers, partners, developers, influencers and the greater global cloud community come to get inspired and learn about Google Cloud Platform.",
+        description: "Next ‘19 will offer more than 500 breakout sessions, led by more than 1,000 Google, customer, and partner speakers. This year, we’ll have even more technical content and learning opportunities. The conference will feature tracks encompassing all aspects of the Google Cloud portfolio: application development, architecture, collaboration and productivity, cost management, data analytics, databases, hybrid cloud, ML and AI, mobility and devices, security, serverless and much more.",
+        lat: 37.7837814,
+        long: -122.4013699
+    },
+    {
+        id: 8,
+        title: "ICML — International Conference on Machine Learning",
+        date: "06/20/2019",
+        location: "300 E Ocean Blvd, Long Beach, CA 90802",
+        image: "../../../assets/conference_images/icml-conference.jpg",
+        summary: "The International Conference on Machine Learning is the premier gathering of professionals dedicated to the advancement of the branch of artificial intelligence.",
+        description: "The International Conference on Machine Learning (ICML) is the premier gathering of professionals dedicated to the advancement of the branch of artificial intelligence known as machine learning. ICML is globally renowned for presenting and publishing cutting-edge research on all aspects of machine learning used in closely related areas like artificial intelligence, statistics and data science, as well as important application areas such as machine vision, computational biology, speech recognition, and robotics. ICML is one of the fastest growing artificial intelligence conferences in the world. Between June 9 and June 15, 2019, at the Long Beach Convention & Entertainment Center in Long Beach, California, ICML will host over 8,000 participants. Participants at ICML span a wide range of backgrounds, from academic and industrial researchers, to entrepreneurs and engineers, to graduate students and postdocs.",
+        lat: 33.766767,
+        long: -118.188774
+    },
+    {
+        id: 9,
+        title: "QCon SF's Netflix Play API",
+        date: "06/24/2019",
+        location: "1535 Broadway, New York, NY 10036",
+        image: "../../../assets/conference_images/netflix-conference.jpg",
+        summary: "At QCon SF, Suudhan Rangarajan presented Netflix Play API: Why We Built an Evolutionary Architecture.",
+        description: "At QCon SF, Suudhan Rangarajan presented Netflix Play API: Why We Built an Evolutionary Architecture. Key takeaways from the talk included: services that have a single identity/responsibility are easier to maintain and upgrade; engineers should spend time identifying core decisions that need to be made when building a service, and determine whether these are Type 1 or Type 2 decisions which require thorough deliberation or rapid experimentation, respectively; and designing an evolutionary architecture, using tools like fitness functions, provides many benefits. Rangarajan, senior software engineer at Netflix, began the presentation by talking about two key business milestones within Netflix in 2018 that also had a large engineering impact. The first was the release of #netflixeverywhere in January, which enabled customers in many countries where Netflix was previously not available to now signup and watch content. The second milestone was the release of new Download & Go functionality in November, which provided the ability for customers to download content to a device for offline viewing. Both of these releases put an increased strain on the Play API that was, amongst other tasks, responsible for starting the streaming of content to customers. This resulted in several service outages, and also a decrease in deployment frequency and an increase in rollbacks, which, as stated by Forsgren, Humble and Kim in their book Accelerate are key metrics correlated with software delivery performance. An overview of the previous architecture of the Play API Service was provided. Customer devices connect to an API Proxy Service running at the edge (presumedly the Zuul API gateway) that communicates with a monolithic API Service which contained several APIs, including the Play API. This API service in turn communicated to domain specific microservices to handle the user's downstream request.",
+        lat: 40.7586143,
+        long: -73.9862046
+    },
+    {
+        id: 10,
+        title: "NG Conf",
+        date: "07/13/2019",
+        location: "University Guest House & Conference Center, Salt Lake City Utah",
+        image: "../../../assets/conference_images/ngconf-conference.jpeg",
+        summary: "ng-conf 2019 will be the Angular event of the year! As always this event will be focused on teaching, educating, and elevating each of the Angular developers in attendance.",
+        description: "ng-conf 2019 will be the Angular event of the year! As always this event will be focused on teaching, educating, and elevating each of the Angular developers in attendance. Always up to date, we will have sessions about the latest version of Angular and related technologies. Whether you are a beginner, intermediate, or expert, this will be the best opportunity we have ever provided. A conference ticket includes three days of intense sessions, short workshops, and non-stop network opportunities. Our two days of optional pre-conference workshops will take your Angular skills into the stratosphere, but they do require a separate ticket. Can't wait to see you there!",
+        lat: 40.775780,
+        long: -111.844657
+    },
+    {
+        id: 11,
+        title: "Ted Talk: AI Smartwatch Seizure Detection",
+        date: "07/15/2019",
+        location: "999 Canada Pl, Vancouver, BC V6C 3C1, Canada",
+        image: "../../../assets/conference_images/reddit-conference.jpg",
+        summary: "MIT Professor Rosalinda Picard discusses her recent development of an AI smartwatch the detects seizures in real time.",
+        description: "Rosalind Picard wrote Affective Computing, a book that outlines how to give machines the skills of emotional intelligence, credited for launching the growing field of affective computing. Named one of seven Tech SuperHeros to Watch by CNN, Picard cofounded two companies that have commercialized technologies invented by her team at MIT: Empatica, which created the first AI-based smartwatch cleared by FDA in Neurology, and Affectiva, which provides emotion AI software. Picard teaches and leads research in affective computing at the MIT Media Lab in Cambridge; works as MIT's faculty chair for MindHandHeart, a campus-wide wellbeing initiative; and serves as chief scientist for Empatica, creating AI-based analytics and wearables to improve human wellbeing.",
+        lat: 49.2884525,
+        long: -123.1118379
+    },
+    {
+        id: 12,
+        title: "IoT World Conference and Expo 2019",
+        date: "07/23/2019",
+        location: "5001 Great America Pkwy, Santa Clara, CA 95054",
+        image: "../../../assets/conference_images/iot-conference.jpg",
+        summary: "Internet of Things World is North America's largest IoT event, bringing you the top technologies, strategies, and case studies for the key industries implementing IoT. ",
+        description: "Internet of Things World is North America's largest IoT event, bringing you the top technologies, strategies, and case studies for the key industries implementing IoT. Over four days, you will meet 12,500+ leaders and innovators from across the IoT community. The ultimate opportunity for you to get out of the office and build valuable connections all in one place. The conference agenda includes keynotes and tracks covering industrial and consumer verticals, and technical focused sessions. Whether you are just starting out or want to scale your IoT projects, there is content designed for you. The expo hall is packed with 300+ exhibitors and startups ready to show you the latest technology solutions to make your IoT dreams a reality. Step into the future of IoT for business.",
+        lat: 37.4018801,
+        long: -121.9777135
+    },
+]
