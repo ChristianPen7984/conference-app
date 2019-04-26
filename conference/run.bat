07142427ef3f6872b122f25f8f0714c415ac00c9
christian212519 <penrod.christian7984@gmail.com>
2019-04-26 18:44:14 -0400

    Created shell script and batch file for local dev execution

---
+++ conference/run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Begining Angular and Dependency Installation
+call npm install
+
+@echo off
+echo Running Angular App on PORT:4200
+call ng serve -o
---
+++ conference/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "\nBegining Angular and Dependency Installation\n"
+npm install
+echo -e "\nRunning Angular App on PORT:4200\n"
+ng serve -o
